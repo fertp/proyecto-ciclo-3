065690745cf3d39704bcd6b3cd004fe0b405081c
Geiner Lara <davidlararada1062@hotmail.com>
2021-09-24 18:48:46 -0500

    componete header creado

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -1,28 +1,25 @@
 <template>
-  <header class="site-header">
+  <header class="site-heeader">
     <div class="site-header__container">
       <div class="header-logo">
-        <p>LOGO</p>
+        <img src="../assets/bicicleta.svg" alt="" class="header-logo__img" />
       </div>
-      <div class="menu-icon">
-        <input class="menu-icon-check" type="checkbox" id="btn-menu" />
-        <label for="btn-menu" class="menu-efecto"
-          ><img :src="image" alt="menu" class="menu-resp"
-        /></label>
-      </div>
-      <nav class="site-nav mostrar">
-        <ul class="site-nav__list">
-          <li class="site-nav__items efecto-items">
+      <nav class="site-nav">
+        <ul :class="[showMenu ? 'site-nav__list--show' : '', 'site-nav__list']">
+          <li class="site-nav__items">
             <router-link to="/">Inicio</router-link>
           </li>
-          <li class="site-nav__items efecto-items">
+          <li class="site-nav__items">
             <router-link to="/productos">Productos</router-link>
           </li>
-          <li class="site-nav__items efecto-items">
+          <li class="site-nav__items">
             <router-link to="/nosotros">Nosotros</router-link>
           </li>
         </ul>
       </nav>
+      <div class="btn_menu" v-on:click="toggleMenu">
+        <img :src="image" alt="icionoResponsi" class="imagenMenu" />
+      </div>
     </div>
   </header>
 </template>
@@ -31,107 +28,72 @@
 body {
   margin: 0;
   padding: 0;
-}
-.site-header {
-  background: #fff;
-  width: 100%;
-  padding: 0 2rem;
   box-sizing: border-box;
-  box-shadow: 0 0 2px #ccc;
+}
+.site-heeader {
+  background: #fff;
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.3);
+  padding: 0 30px;
+  box-sizing: border-box;
+  /* position: relative; */
   position: fixed;
+  width: 100%;
   top: 0;
-}
-.menu-icon .menu-resp {
-  width: 50px;
-  align-items: center;
-  display: none;
-  margin-right: 2rem;
-}
-#btn-menu {
-  display: none;
-}
-.menu-efecto img:hover {
-  /* background: #ccc1cc; */
-  cursor: pointer;
-  width: 55px;
-}
-.site-header a {
-  text-decoration: none;
 }
 .site-header__container {
   display: flex;
-  justify-content: space-between;
-  align-items: center;
   height: 80px;
-  flex-wrap: wrap;
+  margin: 0 auto;
+  max-width: 1280px;
+  align-items: center;
+  justify-content: space-between;
 }
-/* .menu-icon {
-  display: none;
-} */
-.site-nav__list {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-.site-nav__items {
-  display: inline-block;
-}
-.site-nav__items a {
+.header-logo__img {
   display: block;
-  padding: 0.5rem 1rem;
-  transition: all 0.4s linear;
-  border-radius: 15px;
-  color: #000;
-}
-.efecto-items a:hover {
-  background: #ccc1cc;
+  width: 60px;
 }
 
-@media screen and (max-width: 680px) {
-  .site-header {
-    padding: 0;
-  }
-  .site-header a {
-    text-decoration: none;
-  }
-  .site-header__container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    /* height: 80px; */
-  }
-  /* ----------------------------- */
-  .menu-icon .menu-resp {
-    width: 50px;
-    align-items: center;
-    display: block;
-  }
-  .site-nav {
+.site-nav__items a {
+  text-decoration: none;
+}
+.imagenMenu {
+  width: 36px;
+}
+@media screen and (max-width: 639px) {
+  .site-nav__list {
+    list-style: none;
     position: absolute;
+    top: 100%;
+    left: 0;
     width: 100%;
-    margin-top: 11.5rem;
-    background: rgba(34, 34, 34, 0.8);
-    margin: -100%;
-  }
-  .header-logo {
-    margin-left: 2rem;
+    background: rgba(0, 0, 0, 0.3);
+    margin: 0;
+    padding: 0;
+    padding-left: 30px;
+    display: none;
+    height: 100vh;
   }
   .site-nav__items {
-    display: block;
+    padding: 16px 0;
   }
   .site-nav__items a {
-    display: block;
-    padding: 0.5rem 1rem;
-    transition: all 0.4s linear;
-    border-radius: 15px;
     color: #fff;
   }
-  .efecto-items a:hover {
-    background: #ccc1cc;
+  .site-nav__list--show {
+    display: block;
   }
-  #btn-menu:checked ~ .site-nav {
-    position: absolute;
-    margin: 10;
+}
+@media screen and (min-width: 640px) {
+  .btn_menu {
+    display: none;
+  }
+  .site-nav__list {
+    display: flex;
+    list-style: none;
+  }
+  .site-nav__items a {
+    margin-left: 30px;
+    color: #000;
   }
 }
 </style>
@@ -143,7 +105,25 @@ export default {
   name: "PxHeader",
 
   data: function () {
-    return { image: image };
+    return {
+      image: image,
+      showMenu: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
+      console.log(this.showMenu);
+    },
   },
 };
+// addEventListener("DOMContentLoaded", () => {
+//   const btn_menu = document.querySlector(".btn_menu");
+//   if (btn_menu) {
+//     btn_menu.addEventListener("click", () => {
+//       const menu_items = document.querySelector(".site-nav__list");
+//       menu_items.classList.toggle("show");
+//     });
+//   }
+// });
 </script>
