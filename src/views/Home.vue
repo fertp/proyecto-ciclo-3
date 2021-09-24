b4f8cd35b7124a0c02e586ee0be0fe2413638f49
nhoragarciab <nhoragarciab@yahoo.com>
2021-09-24 18:37:55 -0500

    Creamos componente Footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+   <!--  <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <h1>
+      Home
+    </h1>
+    <px-footer>
+
+    </px-footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import PxFooter from "@/components/PxFooter.vue";
+
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    PxFooter
   },
 };
 </script>
