a7924961d64ca93e4821224a718e23a0de8ce6cc
nhoragarciab <nhoragarciab@yahoo.com>
2021-09-27 18:35:45 -0500

    Commit antes de hacer pull a dev

---
+++ src/components/PxTeamCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <article class="team-card">
+    <h3 class="team-card__name">
+      {{member.name}}
+    </h3>
+    <img
+      src="@/assets/Foto_NG.jpg"
+      :alt="member.name"
+      class="team-card__img"
+    />
+    <p class="team-card__description">
+      {{member.description}} 
+    </p>
+    <!-- Esto va dentro de un router-link -->
+    <a href="tp.fernando@gmail.com" class="team-card__button">CONTACTAR</a>
+  </article>
+</template>
+
+<style scoped>
+.team-card {
+  display: block;
+  padding-bottom: 30px;
+  border-radius: 4px;
+  box-shadow: 0 0 2px #ccc;
+  text-align: center;
+}
+.team-card__img {
+  display: block;
+  width: 100%;
+}
+.team-card__button {
+  display: flex;
+  margin: auto;
+  align-items: center;
+  justify-content: center;
+  width: 160px;
+  height: 32px;
+  background-color: #333;
+  color: #fff;
+  font-weight: bold;
+  text-decoration: none;
+  border-radius: 4px;
+}
+.product-card__price {
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
+
+<script>
+export default {
+  name: "PxTeamCard",
+   props: {
+    member: {}, 
+  },
+  // methods: {
+  //   getImagePath(img) {
+  //     return require(`@/assets/${img}`);
+  //   },
+  // },
+};
+</script>
--- src/views/Nosotros.vue
+++ src/views/Nosotros.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about">
+<main class="nosotros-page">
+  <div class="nosotros-page__container">
     <h1>/Nosotros</h1>
+    <div class="row"> 
+   <!-- Fernando -->
+        <div class="column"> 
+            <div class="card"> 
+                <img src="" alt=""> 
+                <div class="container"> 
+                    <h2>Fernando Torres Padilla</h2> 
+                    <p class="title">Digital Marketer</p> 
+                    <p>Desarrollo e implemento campañas de comunicacion en medios digitales.</p> 
+                    <p><a href="mailto:tp.fernando@gmail.com" class="button">Contacto</a></p> 
+                </div> 
+            </div> 
+        </div> 
+
+        <!-- Geiner -->
+        <div class="column"> 
+            <div class="card"> 
+                <img src="" alt="Mike"> 
+                <div class="container"> 
+                    <h2>Geiner Lara Rada</h2> 
+                    <p class="title">Ingeniero </p> 
+                    <p>Desarrollador web y estudiante de ingenieria industrial apasionado por la tecnologia.</p> 
+                    <p><button class="button">Contacto</button></p> 
+                </div> 
+            </div> 
+        </div> 
+
+        <!-- Nhora -->
+        <div class="column"> 
+            <div class="card"> 
+                <img src= "" alt="Mike">
+                <div class="container"> 
+                    <h2>Nhora Yelitza García Bautista</h2> 
+                    <p class="title">Ingeniera Industrial </p> 
+                    <p>Estudiante Mision TIC 2022</p> 
+                    <p><a href="mailto:nhoragarciab@yahoo.com" class="button">Contacto</a></p> 
+                </div> 
+            </div> 
+        </div> 
+      </div>
   </div>
+</main>
 </template>
